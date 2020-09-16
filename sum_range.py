6116f75fbc1051e76590587f8daa9e743d8a95f3
onwuasoanya_george <ndu4george@gmail.com>
2020-09-16 07:16:51 +0100

    added exception

--- sum_range.py
+++ sum_range.py
@@ -1,8 +1,13 @@
-rangeMax = int(input("Enter the max number to sum from zero : \n"))
-numSum = 0
+try:
+    rangeMax = int(input("Enter the max number to sum from zero : \n"))
 
-for num in range(0, (rangeMax + 1)):
-    numSum += num
+    numSum = 0
 
+    for num in range(0, (rangeMax + 1)):
+        numSum += num
 
-print("Total sum of numbers from 0 to ", rangeMax, " is ", numSum)
+    print("Total sum of numbers from 0 to ", rangeMax, " is ", numSum)
+except ValueError:
+    print("Invalid number")
+except:
+    print("Error occured.")
